fb7a79d4a5f346f4ea455b1f23dc65f692e00472
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-03-12 10:48:23 -0700

    finished: functionality finished (open, close, timeout

--- modal.js
+++ modal.js
@@ -1,4 +1,23 @@
-console.log("here");
+document.addEventListener("DOMContentLoaded", function () {
+  const openModalButton = document.getElementById("openModal");
+  const modal = document.getElementById("modal");
+
+    // > Open modal on button click
+    openModalButton.addEventListener("click", function () {
+      modal.classList.remove("invisible");
+      // > Timeout to close modal after 5 seconds
+      setTimeout(() => {
+        modal.classList.add("invisible");
+      }, 5000);
+    });
+
+  // > Close modal on esc press
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      modal.classList.add("invisible");
+    }
+  });
+});
 
 // select button element
 
